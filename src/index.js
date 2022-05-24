34d33850c7b628b1f2c400e157306c9abddf5afa
kylemontgomery1 <kylemontgomery@wustl.edu>
2022-05-23 20:22:52 -0500

    deleted index.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import validWords from "./validwords";
 import wordList from "./wordlist";
